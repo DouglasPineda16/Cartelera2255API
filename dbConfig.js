43c2a7a5c03c567aac51e8f740b3bbca07ee884a
DouglasPineda16 <dpinedah3@miumg.edu.gt>
2025-09-27 15:34:28 -0600

    Api

--- dbConfig.js
+++ dbConfig.js
@@ -1,14 +1,17 @@
 // dbConfig.js
+require('dotenv').config();
+
 const sql = require('mssql');
 
 const config = {
-  user: 'usr_DesaWebDevUMG',
-  password: '!ngGuast@360',
-  server: 'svr-sql-ctezo.southcentralus.cloudapp.azure.com',
-  database: 'db_DesaWebDevUMG',
+  user: process.env.DB_USER || 'usr_DesaWebDevUMG',
+  password: process.env.DB_PASS || '!ngGuast@360',
+  server: process.env.DB_SERVER || 'svr-sql-ctezo.southcentralus.cloudapp.azure.com',
+  database: process.env.DB_NAME || 'db_DesaWebDevUMG',
   options: { encrypt: true, trustServerCertificate: true },
   pool: { max: 10, min: 0, idleTimeoutMillis: 30000 }
 };
+
 
 const poolPromise = new sql.ConnectionPool(config)
   .connect()
